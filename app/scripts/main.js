42128dab383123db5eba34fba11ad57c3d280097
Glenn Simon <glenncochransimon@gmail.com>
2021-04-07 14:23:40 -0700

    modified messaging const in main.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -87,7 +87,8 @@ const puzzleWorker = (function() {
   const concessionBtn = document.getElementById('concessionBtn');
   const firebase = window.firebase;
   const db = firebase.firestore();
-  const messaging = firebase.messaging();
+  const messaging = firebase.messaging.isSupported ?
+      firebase.messaging() : null;
   const scoreValues = {
     A: 1, B: 4, C: 4, D: 2, E: 1, F: 4, G: 3, H: 4, I: 1, J: 10, K: 5, L: 2,
     M: 4, N: 2, O: 1, P: 4, Q: 10, R: 1, S: 1, T: 1, U: 2, V: 5, W: 4, X: 8,
