8b701f87d1fc5071858c112ea4a5d50f978488ed
Glenn Simon <glenncochransimon@gmail.com>
2022-09-30 11:46:39 -0700

    flushed files in updated .gitignore

--- dist/view.c981a86385cd7bf75e33.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-(self["webpackChunk"] = self["webpackChunk"] || []).push([["view"],{},
-/******/ __webpack_require__ => { // webpackRuntimeModules
-/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-/******/ __webpack_require__.O(0, ["controller","init","router","vendors-node_modules_firebaseui_dist_esm_js-node_modules_firebaseui_dist_firebaseui_css","signin"], () => (__webpack_exec__("./src/view.js")));
-/******/ var __webpack_exports__ = __webpack_require__.O();
-/******/ }
-]);
